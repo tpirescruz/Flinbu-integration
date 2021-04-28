c8f668c5d7c7514196208ef08d6ec87cb60a7b7d
BeTr4y4l <75473312+BeTr4y4l@users.noreply.github.com>
2021-04-28 22:07:27 +0200

    second commit with basic JS menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-const burger = document.querySelector('.menu');
+// Menu 
+
+const toggleMenu = document.querySelector(".main-nav__toggler");
+const menu = document.querySelector(".menu-list");
+
+toggleMenu.addEventListener("click", function() {
+    const open = JSON.parse(toggleMenu.getAttribute("aria-expanded"));
+    toggleMenu.setAttribute("aria-expanded", !open);
+    menu.toggle = !menu.toggle;
+});
